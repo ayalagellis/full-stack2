94f7af0b73b3b4a421bd974c7cc90ab8481b3659
Rivkamelka <100512093+Rivkamelka@users.noreply.github.com>
2024-02-11 09:50:26 +0200

    Add files via upload

---
+++ Project_2AyalaRivka/script.js/script.js
@@ -0,0 +1,54 @@
+function loginUser(){
+    var storedName = localStorage.getItem('uname');
+    var storedPw = localStorage.getItem('psw');
+
+    var userName = document.getElementById('uname');
+    var userPw = document.getElementById('psw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('You are logged in.');
+    }else{
+        alert('Error on login');
+    }
+}
+
+    function signUpUser() {
+        var name = document.getElementById('signup-name');
+        var lastName = document.getElementById('signup-lastName');
+        var username = document.getElementById('signup-username');
+        var pw = document.getElementById('signup-psw');
+        var confirmPw = document.getElementById('confirm-psw');
+        var lowerCaseLetters = /[a-z]/g;
+       var upperCaseLetters = /[A-Z]/g;
+       var numbers = /[0-9]/g;
+
+    if(name.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Please fill in email and password');
+
+    }else if(pw.value.length > 8){
+        alert('Max of 8');
+
+    }else if(!pw.value.match(numbers)){
+        alert('please add 1 number');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('please add 1 uppercase letter');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('please add 1 lovercase letter');
+
+    }else{
+        localStorage.setItem('uname', name.value);
+        localStorage.setItem('psw', pw.value);
+        alert('Your account has been created');
+    }
+}
+
+
